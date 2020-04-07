6543f3c89fe69e51a40740b06328fd6f91578508
Daniel Afonso <daniel.afonso@sage.com>
2020-04-06 21:35:16 -0300

    settings heroku

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'amldzbc_bzmfahc(3sw952#bz8hy&pzkqq*c0h-vlc4rr5g10b'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
